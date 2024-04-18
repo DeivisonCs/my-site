029f45f9be7f10ccdd021a21e92eab3c3012de36
DeivisonSantos <deivisonddj3@gmail.com>
2024-04-18 00:01:31 -0300

    animation fixed (i hope so)

--- src/components/AboutMeSection/index.jsx
+++ src/components/AboutMeSection/index.jsx
@@ -6,70 +6,50 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 const AboutMeSection = () => {
 
     const aboutRef = useRef(); 
-    const timeLine = useRef(); 
+    // const timeLine = useRef(); 
 
     useLayoutEffect(() => {
-
         gsap.registerPlugin(ScrollTrigger);
-        gsap.context(() => {
+        const timeline = gsap.timeline({
+            scrollTrigger: {
+                trigger: aboutRef.current,
+                scrub: true,
+                markers:true,
+                start: "top 50%",
+                end: "top 10%"
+            }
+        });
 
-            timeLine.current = gsap.timeline({
-                scrollTrigger: {
-                    trigger: ".AboutMeContent",
-                    scrub: true,
-                    // markers: true,
-                    start: "top 80%",
-                    end: "top 55%"
-                }
-            })
-            .fromTo("#about-me-title", {
-                opacity: 0,
-                y: 200
-            }, {
-                opacity: 1,
-                y: 0
-            })
-            .fromTo("#about-me-underline", {
-                opacity: 0,
-                y: 200
-            }, {
-                opacity: 1,
-                y: 0
-            })
-            .fromTo("#about-me-text", {
-                opacity: 0,
-                y: 200
-            }, {
-                opacity: 1,
-                y: 0
-            })
-            .fromTo("#text_1", {
-                opacity: 0,
-                x: 100
-            }, {
-                opacity: 1,
-                x: 0
-            })
-            .fromTo("#text_2", {
-                opacity: 0,
-                letterSpacing: 100
-            }, {
-                opacity: 1,
-                letterSpacing: 0
-            })
-            .fromTo("#text_3", {
-                opacity: 0,
-                x: -100
-            }, {
-                opacity: 1,
-                x: 0
-            })
-        }, aboutRef)
+        animateTimeline(timeline);
 
-        return(() => {
-            gsap.killTweensOf(".AboutMeContent");
-        })
+        return () => {
+            timeline.kill();
+        };
     }, []);
+
+    const animateTimeline = (timeline) => {
+        timeline.fromTo("#about-me-title, #about-me-underline", {
+            opacity: 0,
+            y: 200
+        }, {
+            opacity: 1,
+            y: 0,
+            stagger: 0.2
+        }).fromTo("#about-me-text", {
+            opacity: 0,
+            x: 200
+        }, {
+            opacity: 1,
+            x: 0
+        }).fromTo(".about-me-text", {
+            opacity: 0,
+            x: -100
+        }, {
+            opacity: 1,
+            x: 0,
+            stagger: 0.2
+        });
+    };
 
 
     return(
@@ -78,14 +58,14 @@ const AboutMeSection = () => {
             <span className="AboutMeContent" id="about-me-underline"></span>
 
             <div className="AboutMeContent" id="about-me-text">
-                <p id="text_1">
+            <p className="AboutMeContent about-me-text">
                     Hi, I'm Deivison Cassimiro dos Santos. I'm always trying to learn something new and that's inspired me to study programming. I live in Brazil and I started my career at the college, beside that I always try to do some courses to improve my knowledge.
                 </p> 
-                <p id="text_2">
+                <p className="AboutMeContent about-me-text">
                     I started learning <mark>C</mark> at the college and study <mark>JavaScript</mark>, <mark>HTML</mark> and <mark>CSS</mark> on my own. Later that year a professor invited me to work on a project for the university itself, which made me to study <mark>Python</mark> to deal with this new challenge. Unfortunately, the project was stopped, and I couldn't finish it, but learned a lot working there.
                 </p>
-                <p id="text_3">
-                    I want to work as a Full-Stack developer or Mobile Developer, but I know there's a long journey  ahead, but right now, I just wanna learn everything I can to become a better professional
+                <p className="AboutMeContent about-me-text">
+                    I want to work as a Full-Stack developer or Mobile Developer, but I know there's a long journey ahead, but right now, I just wanna learn everything I can to become a better professional
                 </p>
             </div>
         </AboutMeDiv>
